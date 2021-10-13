dcc436064ec078d2c724654461a142727b23f2cb
himanshujain <himanshujain@gmail.com>
2021-10-13 12:14:17 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser=require("body-parser");
 const port=3000;
 
 let items=[];
+let workItems=[];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -20,10 +21,25 @@ app.get("/",function(req,res){
 res.render("list",{kindOfDay:day,listItems:items});
 });
 
+app.get("/about",function(req,res){
+  res.render("about");
+});
+
 app.post("/",function(req,res){
   let item=req.body.newitem;
-  items.push(item);
- res.redirect("/");
+  if(req.body.button==="Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else
+  {
+   items.push(item);
+   res.redirect("/");
+}
+});
+
+app.get("/work",function(req,res){
+  res.render("list",{kindOfDay:"Work List",listItems:workItems});
 });
 
 app.listen(port,function(){
